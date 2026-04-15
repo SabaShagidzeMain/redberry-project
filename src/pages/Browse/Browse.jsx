1cdba7e135e73b29bd97244507f513c3726783e5
SabaShagidzeMain <sabadoeswebdev@gmail.com>
2026-04-15 08:25:53 +0400

    feat: filters UI

--- src/pages/Browse/Browse.jsx
+++ src/pages/Browse/Browse.jsx
@@ -11,15 +11,96 @@ export default function Browse() {
         &gt; <span>Browse</span>
       </div>
 
-      {/* TOOLBAR */}
       <div className={styles.toolbar}>
-        {/* LEFT: filters */}
         <div className={styles.filters}>
-          <div>Categories</div>
-          <div>Topics</div>
-          <div>Instructors</div>
+          <div className={styles.filtersHeader}>
+            <h4>Filters</h4>
+            <button>Clear all filters</button>
+          </div>
+          <div className={styles.tagContainer}>
+            <div className={styles.tagHeader}>
+              <p>Categories</p>
+            </div>
+            <div className={styles.tagWrapper}>
+              <div className={styles.tag}>
+                <p>Development</p>
+              </div>
+              <div className={styles.tag}>
+                <p>Design</p>
+              </div>
+              <div className={styles.tag}>
+                <p>Business</p>
+              </div>
+              <div className={styles.tag}>
+                <p>Data Science</p>
+              </div>
+              <div className={styles.tag}>
+                <p>Marketing</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.tagContainer}>
+            <div className={styles.tagHeader}>
+              <p>Topics</p>
+            </div>
+            <div className={styles.tagWrapper}>
+              <div className={styles.tag}>
+                <p>React</p>
+              </div>
+              <div className={styles.tag}>
+                <p>TypeScript</p>
+              </div>
+              <div className={styles.tag}>
+                <p>Phython</p>
+              </div>
+              <div className={styles.tag}>
+                <p>UX/UI</p>
+              </div>
+              <div className={styles.tag}>
+                <p>Figma</p>
+              </div>
+              <div className={styles.tag}>
+                <p>JavaScript</p>
+              </div>
+              <div className={styles.tag}>
+                <p>Node.js</p>
+              </div>
+              <div className={styles.tag}>
+                <p>Machine Learning</p>
+              </div>
+              <div className={styles.tag}>
+                <p>SEO</p>
+              </div>
+              <div className={styles.tag}>
+                <p>Analytics</p>
+              </div>
+            </div>
+          </div>
 
-          <div className={styles.activeFilters}>3 active filters</div>
+          <div className={styles.tagContainer}>
+            <div className={styles.tagHeader}>
+              <p>Instructor</p>
+            </div>
+            <div className={styles.instructorWrapper}>
+              <div className={styles.tag}>
+                <p>Marilyn Mango</p>
+              </div>
+              <div className={styles.tag}>
+                <p>Ryan Dorwart</p>
+              </div>
+              <div className={styles.tag}>
+                <p>Roger Calzoni</p>
+              </div>
+              <div className={styles.tag}>
+                <p>Zain Philips</p>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.activeFilters}>
+            <span></span>
+            <p>0 Filters Active</p>
+          </div>
         </div>
 
         {/* RIGHT: course controls */}
