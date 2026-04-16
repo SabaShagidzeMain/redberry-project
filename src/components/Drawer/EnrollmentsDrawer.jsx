1ed208a233776ec06458cd93f951a7a492cfafe2
Saba Shagidze <sabadoeswebdev@gmail.com>
2026-04-16 06:57:10 +0400

    feat: full working sidebar with base styling

--- src/components/Drawer/EnrollmentsDrawer.jsx
+++ src/components/Drawer/EnrollmentsDrawer.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react";
 import styles from "./EnrollmentsDrawer.module.css";
 import { enrollmentApi } from "../../api/enrollments.api";
+import calendar from "../../assets/icons/callendar.png";
+import clock from "../../assets/icons/clock.png";
+import user from "../../assets/icons/person.png";
+import location from "../../assets/icons/location.png";
+import { Link } from "react-router-dom";
 
 export default function EnrollmentsDrawer({ open, onClose }) {
   const [enrollments, setEnrollments] = useState([]);
@@ -40,7 +45,7 @@ export default function EnrollmentsDrawer({ open, onClose }) {
         <div className={styles.header}>
           <h3>Enrolled Courses</h3>
           <p>
-            Total Enrollments:{" "}
+            Total Enrollments:
             <span className={styles.count}>{enrollments.length}</span>
           </p>
         </div>
@@ -54,19 +59,55 @@ export default function EnrollmentsDrawer({ open, onClose }) {
 
           {enrollments.map((e) => (
             <div key={e.id} className={styles.card}>
-              <img src={e.course.image} alt="" />
-
-              <div>
-                <h4>{e.course.title}</h4>
-
-                <p>
-                  {e.schedule.weeklySchedule.label} •{" "}
-                  {e.schedule.timeSlot.label}
-                </p>
-
-                <p>Progress: {e.progress}%</p>
-
-                <p>Total: ${e.totalPrice}</p>
+              <div className={styles.cardTop}>
+                <div>
+                  <img src={e.course.image} alt="" />
+                </div>
+                <div className={styles.info}>
+                  <div className={styles.infoFirst}>
+                    <p className={styles.instructor}>
+                      {e.course.instructor.name}
+                    </p>
+                    <p>⭐ {e.course.avgRating}</p>
+                  </div>
+                  <h4>{e.course.title}</h4>
+                  <div className={styles.infoList}>
+                    <div className={styles.infoListItem}>
+                      <img src={calendar} alt="" />
+                      <p>{e.schedule.weeklySchedule.label}</p>
+                    </div>
+                    <div className={styles.infoListItem}>
+                      <img src={clock} alt="" />
+                      <p>{e.schedule.timeSlot.label}</p>
+                    </div>
+                    <div className={styles.infoListItem}>
+                      <img src={user} alt="" />
+                      <p>{e.schedule.sessionType.name}</p>
+                    </div>
+                    <div className={styles.infoListItem}>
+                      {e.schedule.location && (
+                        <>
+                          <img src={location} alt="" />
+                          <p> {e.schedule.location}</p>
+                        </>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.cardBot}>
+                <div className={styles.botLeft}>
+                  <p>Progress: {e.progress}%</p>
+                  <div className={styles.progressBar}>
+                    <div
+                      className={styles.progressFill}
+                      style={{ width: `${e.progress}%` }}
+                    />
+                  </div>
+                </div>
+                <div className={styles.cardButton} onClick={onClose}>
+                  <Link to={`/course/${e.course.id}`}>View</Link>
+                </div>
               </div>
             </div>
           ))}
