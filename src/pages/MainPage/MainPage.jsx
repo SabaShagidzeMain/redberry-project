7f850caed266a82c38e057980a9772cfafa50d2e
Saba Shagidze <sabadoeswebdev@gmail.com>
2026-04-16 11:16:00 +0400

    feat: create main page route

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+import styles from "./MainPage.module.css";
+
+export default function MainPage() {
+    return(
+        <div>
+            Main Page
+        </div>
+    )
+}
